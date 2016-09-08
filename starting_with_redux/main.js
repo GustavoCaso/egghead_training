3f4ecd8817cdd6a209b4ca9e79614e9b592c3fc0
GustavoCaso <gustavocaso@gmail.com>
2016-09-08 08:42:37 +0200

    Implement createStore from scratch

--- starting_with_redux/main.js
+++ starting_with_redux/main.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -11,6 +11,37 @@ const counter = (state = 0, action) => {
   }
 }
 
+// createStore receive a reducer created by the application
+// and return thre fuctions getState, dispatch, subscribe
+const createStore = (reducer) => {
+  let state;
+  // to keep track of all the time the subscribe function is call
+  let listeners = [];
+
+  // return the current vale of state
+  const getState = () => state;
+
+
+  const dispatch = (action) => {
+    // only way to change internal state
+    state = reducer(state, action);
+    // everytime the state is change we need to notify every listener and call them
+    listeners.forEach(listener => listener());
+  }
+
+  const subscribe = (listener) => {
+    // every time its call we store the listener in our listeners array
+    listeners.push(listener);
+    // we have provide an unsubcribe a listener, for that we will return a function that remove this listener from the listeners array
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    };
+  };
+  // return the initial state
+  dispatch({});
+
+  return {getState, dispatch, subscribe}
+}
 const store = createStore(counter);
 
 const render = () => {
