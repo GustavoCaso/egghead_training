9fa98bc5c72d546eb98741e5dbf11a6777607355
GustavoCaso <gustavocaso@gmail.com>
2016-09-09 08:23:18 +0200

    Understanding Reducer Composition with array

--- starting_with_redux/main.js
+++ starting_with_redux/main.js
@@ -4,28 +4,38 @@ import ReactDOM from 'react-dom'
 import deepFreeze from 'deep-freeze'
 import expect from 'expect'
 
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        complete: false
+      }
+
+    case 'TOGGLE_TODO':
+      if(state.id == action.id){
+        return Object.assign(
+          {}, state,
+          {complete: !state.complete}
+        )
+      }else{
+        return state
+      }
+    default:
+      return state;
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          complete: false
-        }
+        todo(undefined, action)
       ]
     case 'TOGGLE_TODO':
-      return state.map(todo =>{
-        if(todo.id == action.id){
-          return Object.assign(
-            {}, todo,
-            {complete: !todo.complete}
-          )
-        }else{
-          return todo
-        }
-      })
+      return state.map(t => todo(t, action))
     default:
       return state;
   }
@@ -33,71 +43,71 @@ const todos = (state = [], action) => {
 
 
 
-const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  }
-  const stateAfter = [
-    {
+  const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+      type: 'ADD_TODO',
       id: 0,
-      text: 'Learn Redux',
-      complete: false
+      text: 'Learn Redux'
     }
-  ]
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        complete: false
+      }
+    ]
 
-  deepFreeze(stateBefore)
-  deepFreeze(action)
+    deepFreeze(stateBefore)
+    deepFreeze(action)
 
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
-}
-
-const testToggleTodo = () => {
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      complete: false
-    },
-    {
-      id: 1,
-      text: 'Shop for the cat',
-      complete: false
-    }
-
-  ];
-
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 0
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter);
   }
 
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      complete: true
-    },
-    {
-      id: 1,
-      text: 'Shop for the cat',
-      complete: false
+  const testToggleTodo = () => {
+    const stateBefore = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        complete: false
+      },
+      {
+        id: 1,
+        text: 'Shop for the cat',
+        complete: false
+      }
+
+    ];
+
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 0
     }
 
-  ]
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        complete: true
+      },
+      {
+        id: 1,
+        text: 'Shop for the cat',
+        complete: false
+      }
 
-  deepFreeze(action)
-  deepFreeze(stateBefore)
+    ]
 
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter)
-}
+    deepFreeze(action)
+    deepFreeze(stateBefore)
 
-testAddTodo();
-testToggleTodo();
-console.log('All tests passed');
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter)
+  }
+
+  testAddTodo();
+  testToggleTodo();
+  console.log('All tests passed');
