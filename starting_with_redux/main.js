58e0788671d13c6bf14c5804e99c207d4f0ec880
GustavoCaso <gustavocaso@gmail.com>
2016-09-23 07:16:46 +0200

    Use Connect to create the component by omiting all the boilerplate

--- starting_with_redux/main.js
+++ starting_with_redux/main.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux';
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -223,44 +224,28 @@ const getVisibleTodos = (
   }
 }
 
-class VisibleTodoList extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <TodoList
-        todos={
-          getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-          )
-        }
-        onTodoClick={id =>
-          store.dispatch({
-            type: 'TOGGLE_TODO',
-            id
-          })
-        }
-      />
-    );
-  }
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+        state.todos,
+        state.visibilityFilter)
+  };
 }
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
-};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) =>
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+  };
+}
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
 
 const TodoApp = () => (
   <div>
@@ -269,21 +254,6 @@ const TodoApp = () => (
     <Footer />
   </div>
 );
-
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
