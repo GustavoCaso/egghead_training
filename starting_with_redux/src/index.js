34a4591abc1921131ab7781712f346b7455302da
GustavoCaso <gustavocaso@gmail.com>
2016-09-29 07:50:03 +0200

    Use throttle function to avoid to much call to saveState

--- starting_with_redux/src/index.js
+++ starting_with_redux/src/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
 
 const persistedState = loadState();
 const store = createStore(
@@ -13,11 +14,11 @@ const store = createStore(
   persistedState
 );
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   });
-})
+}, 1000));
 
 render(
   <Provider store={store}>
