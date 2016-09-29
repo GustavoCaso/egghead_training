640e65824544537f3efa1a4bcb6db3f6c379ea48
GustavoCaso <gustavocaso@gmail.com>
2016-09-29 07:46:42 +0200

    Storage and load previous state from localStorage, using Redux createStore second argument

--- starting_with_redux/src/index.js
+++ starting_with_redux/src/index.js
@@ -5,8 +5,19 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './localStorage'
 
-const store = createStore(todoApp);
+const persistedState = loadState();
+const store = createStore(
+  todoApp,
+  persistedState
+);
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+})
 
 render(
   <Provider store={store}>
