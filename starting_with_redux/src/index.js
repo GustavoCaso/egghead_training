afea90d01c37866b033d217734879f663093971c
GustavoCaso <gustavocaso@gmail.com>
2016-10-02 09:01:21 +0200

    Created fake backend

--- starting_with_redux/src/index.js
+++ starting_with_redux/src/index.js
@@ -2,7 +2,12 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
-import configureStore from './configureStore'
+import configureStore from './configureStore';
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos =>
+  console.log(todos) // eslint-disable-line no-console
+);
 
 const store = configureStore()
 
