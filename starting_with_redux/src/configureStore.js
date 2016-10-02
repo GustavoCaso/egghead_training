d331ab81329817aad21ba28cbcfa0f0c88b5633a
GustavoCaso <gustavocaso@gmail.com>
2016-10-02 10:50:09 +0200

    Understand the concept of middlewares, and how applying curry function will help us defining middlewares

--- starting_with_redux/src/configureStore.js
+++ starting_with_redux/src/configureStore.js
@@ -1,44 +1,46 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingMiddleware = (store) => {
-  const rawDispatch = store.dispatch;
+const logging = (store) => (next) => {
   if (!console.group) {
-    return rawDispatch
+    return next
   }
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state','color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue
-  }
-}
+  };
+};
 
-const addPromiseSupportForStore = (store) => {
-  const rawDispatch = store.dispatch;
-
-  return (action) => {
-    if(typeof action.then === 'function'){
-      return action.then(rawDispatch)
-    }
-      return rawDispatch(action)
+const promises = (store) => (next) => (action) => {
+  if(typeof action.then === 'function'){
+    return action.then(next)
   }
+    return next(action);
+};
+
+const wrapStoreDispatchWithMiddleware = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  );
 }
 
 
 const configureStore = () => {
+  const middlewares = [promises]
   const store = createStore(
     todoApp,
   );
 
   if (process.env.NODE_ENV != 'production'){
-    store.dispatch = addLoggingMiddleware(store)
+    middlewares.push(logging)
   }
 
-  store.dispatch = addPromiseSupportForStore(store)
+  wrapStoreDispatchWithMiddleware(store, middlewares)
 
   return store;
 }
