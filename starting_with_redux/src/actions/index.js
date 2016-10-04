08a72fd320f66ac7eac0622a6b54dccbaa04d23d
GustavoCaso <gustavocaso@gmail.com>
2016-10-04 07:46:51 +0200

    Added loading feature by modifying the reducers of listByFilter to add a attribute isFetching to each list, and adding a new action REQUEST_TODOS

--- starting_with_redux/src/actions/index.js
+++ starting_with_redux/src/actions/index.js
@@ -7,6 +7,12 @@ const receiveTodos = (filter, response) => ({
   response,
 })
 
+export const requestTodos = (filter) => ({
+  type: 'REQUEST_TODOS',
+  filter
+})
+
+
 export const fetchTodos = (filter) =>
   api.fetchTodos(filter).then(response =>
     receiveTodos(filter, response)
