e98d43cb58c449c591d0354482034d6f3446843b
GustavoCaso <gustavocaso@gmail.com>
2016-10-05 07:05:24 +0200

    Update actions and reducers to handle error case when fetching data

--- starting_with_redux/src/actions/index.js
+++ starting_with_redux/src/actions/index.js
@@ -2,26 +2,30 @@ import { v4 } from 'node-uuid'
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
-const receiveTodos = (filter, response) => ({
-  type: 'RECEIVE_TODOS',
-  filter,
-  response,
-})
-
-const requestTodos = (filter) => ({
-  type: 'REQUEST_TODOS',
-  filter
-})
-
-
 export const fetchTodos = (filter) => (dispatch, getState) => {
   if (getIsFetching(getState(), filter)){
     return Promise.resolve();
   }
-  dispatch(requestTodos(filter));
+  dispatch({
+    type: 'FETCH_TODOS_REQUEST',
+    filter
+  });
 
-  return api.fetchTodos(filter).then(response =>
-    dispatch(receiveTodos(filter, response))
+  return api.fetchTodos(filter).then(
+    response => {
+      dispatch({
+        type: 'FETCH_TODOS_SUCCESS',
+        filter,
+        response,
+      });
+    },
+    error => {
+      dispatch({
+        type: 'FETCH_TODOS_FAILURE',
+        filter,
+        error: error.message || 'Something went wrong'
+      });
+    }
   );
 }
 
